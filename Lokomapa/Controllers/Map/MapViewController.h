0015f17730fe510e2a82ff5f2597e68e261e8236
Lukasz Domaradzki <lukasz.domaradzki@gmail.com>
2013-09-24 23:53:30 +0200

    Initial map view, project cleanup

---
+++ Lokomapa/Controllers/Map/MapViewController.h
@@ -0,0 +1,16 @@
+//
+//  MapViewController.h
+//  Lokomapa
+//
+//  Created by ldomaradzki on 24.09.2013.
+//  Copyright (c) 2013 ldomaradzki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface MapViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
+
+@end
---
+++ Lokomapa/Controllers/Map/MapViewController.m
@@ -0,0 +1,38 @@
+//
+//  MapViewController.m
+//  Lokomapa
+//
+//  Created by ldomaradzki on 24.09.2013.
+//  Copyright (c) 2013 ldomaradzki. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@interface MapViewController ()
+
+@end
+
+@implementation MapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
